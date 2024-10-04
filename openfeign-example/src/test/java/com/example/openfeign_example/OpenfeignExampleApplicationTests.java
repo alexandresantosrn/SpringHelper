b2f8ca41b8c54e1890326c76d90d3697fa2dbe75
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2024-10-03 23:10:20 -0300

    Commit inicial projeto.

---
+++ openfeign-example/src/test/java/com/example/openfeign_example/OpenfeignExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openfeign_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeignExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
